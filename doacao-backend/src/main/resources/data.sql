dcaca608a277456ab4c4dbf7ba72ad21f0e54644
Rafael Porreca <rafael.porreca@gmail.com>
2021-11-06 13:00:44 -0300

    Carga inicial do banco de dados para teste

---
+++ doacao-backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_estado (nome) VALUES ('Rio de Janeiro');
+INSERT INTO tb_estado (nome) VALUES ('São Paulo');
+
+INSERT INTO tb_municipio (nome,estado_id) VALUES ('Rio de Janeiro',1);
+INSERT INTO tb_municipio (nome,estado_id) VALUES ('Cabo frio',1);
+INSERT INTO tb_municipio (nome,estado_id) VALUES ('São Paulo',2);
+INSERT INTO tb_municipio (nome,estado_id) VALUES ('Campinas',2);
