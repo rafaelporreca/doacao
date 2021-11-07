2cb57b6193c1f657cc58c15537e7d3395626f5f2
Rafael Porreca <rafael.porreca@gmail.com>
2021-11-07 10:28:42 -0300

    Listadno estados

---
+++ front-web/src/components/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="estado of estados">{{ estado.nome }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            estados: []
+        }
+    },
+    created() {
+        this.$http.get('http://localhost:8080/estados')
+        .then(res => res.json())
+        .then(estados => this.estados = estados, err => console.log(err));
+    } 
+  }
+</script>
+
+<style lang="scss">
+
+</style>
